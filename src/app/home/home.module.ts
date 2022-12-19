b44d9244876b8c5c60cdfe86f8240846ac055f74
Adeilson Silva <adeilson.silva@agenciagenia.com.br>
2022-12-19 10:34:34 -0300

    Integration with Dialogflow done

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
@@ -16,6 +16,7 @@ import { AnnouncementComponent } from './announcement/announcement.component';
   imports: [
     CommonModule,
     HomeRoutingModule
-  ]
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class HomeModule { }
